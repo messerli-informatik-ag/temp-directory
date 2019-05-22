d486265753268309d1baed1a9e4077eac102bb6e
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-22 16:55:57 +0200

    Implement builder

--- TempDirectory/TempDirectoryBuilder.cs
+++ TempDirectory/TempDirectoryBuilder.cs
@@ -1,42 +1,70 @@
+using System;
 using System.IO;
 
 namespace Messerli.TempDirectory
 {
     public class TempDirectoryBuilder : ITempDirectoryBuilder
     {
+        private string _prefix = "";
+        private string _suffix = "";
+        private string _prefixSeparator = "-";
+        private string _suffixSeparator = "-";
+
+        public TempDirectoryBuilder()
+        {
+        }
+
+        private TempDirectoryBuilder(string prefix, string suffix, string prefixSeparator, string suffixSeparator)
+        {
+            _prefix = prefix;
+            _suffix = suffix;
+            _prefixSeparator = prefixSeparator;
+            _suffixSeparator = suffixSeparator;
+        }
+
         public ITempDirectoryBuilder Prefix(string prefix)
         {
-            throw new System.NotImplementedException();
+            _prefix = prefix;
+            return DeepClone();
         }
 
         public ITempDirectoryBuilder PrefixSeparator(string prefixSeparator)
         {
-            throw new System.NotImplementedException();
+            _prefixSeparator = prefixSeparator;
+            return DeepClone();
         }
 
         public ITempDirectoryBuilder Suffix(string suffix)
         {
-            throw new System.NotImplementedException();
+            _suffix = suffix;
+            return DeepClone();
         }
 
         public ITempDirectoryBuilder SuffixSeparator(string suffixSeparator)
         {
-            throw new System.NotImplementedException();
+            _suffixSeparator = suffixSeparator;
+            return DeepClone();
         }
 
         public TempDirectory Create()
         {
-            throw new System.NotImplementedException();
-        }
-
-        private static string CreateTempDirectory(string directoryName)
-        {
             var tempPath = Path.GetTempPath();
+            var guid = Guid.NewGuid().ToString();
+            var prefixSubstring = string.IsNullOrEmpty(_prefix) ? "" : _prefix + _prefixSeparator;
+            var suffixSubstring = string.IsNullOrEmpty(_suffix) ? "" : _suffix + _suffixSeparator;
+
+            var directoryName = $"{prefixSubstring}{guid}{suffixSubstring}";
             var path = Path.Combine(tempPath, directoryName);
 
             Directory.CreateDirectory(path);
+            var onDispose = CreateDirectoryDeletionFn(path);
 
-            return path;
+            return new TempDirectory(directoryName, path, onDispose);
+        }
+
+        private ITempDirectoryBuilder DeepClone()
+        {
+            return new TempDirectoryBuilder(_prefix, _suffix, _prefixSeparator, _suffixSeparator);
         }
 
         private static OnDispose CreateDirectoryDeletionFn(string directoryPath)
