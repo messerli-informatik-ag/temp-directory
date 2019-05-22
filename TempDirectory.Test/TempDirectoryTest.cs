f5c0d752781842946411c2171e50616afe0c8eb1
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-22 10:09:47 +0200

    Port temp directory implementation

---
+++ TempDirectory.Test/TempDirectoryTest.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using Xunit;
+using TempDir = Messerli.TempDirectory.TempDirectory;
+
+namespace TempDirectory.Test
+{
+    public class TempDirectoryTest
+    {
+        private const string tempDirectoryName = "temp_directory_test";
+
+        [Fact]
+        public void CreatesTempDirectory()
+        {
+            using (var generateFileStructure = new TempDir(tempDirectoryName))
+            {
+                var path = generateFileStructure.Path;
+                Assert.True(Directory.Exists(path));
+            }
+        }
+
+        [Fact]
+        public void RemovesTempDirectory()
+        {
+            var generateFileStructure = new TempDir(tempDirectoryName);
+            var path = generateFileStructure.Path;
+            Assert.True(Directory.Exists(path));
+
+            generateFileStructure.Dispose();
+
+            Assert.False(Directory.Exists(path));
+        }
+    }
+}
---
+++ TempDirectory/TempDirectory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Messerli.TempDirectory
+{
+    public class TempDirectory : IDisposable
+    {
+        public string Path { get; }
+
+        public TempDirectory(string directoryName)
+        {
+            try
+            {
+                Path = CreateTempDirectory(directoryName);
+            }
+            catch (Exception)
+            {
+                Dispose();
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(Path, true);
+        }
+
+        private static string CreateTempDirectory(string directoryName)
+        {
+            var tempPath = System.IO.Path.GetTempPath();
+            var path = System.IO.Path.Combine(tempPath, directoryName);
+
+            Directory.CreateDirectory(path);
+
+            return path;
+        }
+
+
+    }
+}
