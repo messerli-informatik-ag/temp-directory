beb07c7811adf29ae323cb55e8071243af096f32
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-05-22 12:35:20 +0200

    Add test data

--- TempDirectory.Test/TempDirectoryBuilderTest.cs
+++ TempDirectory.Test/TempDirectoryBuilderTest.cs
@@ -1,34 +1,69 @@
+using System.Collections.Generic;
 using System.IO;
 using Messerli.TempDirectory;
 using Xunit;
+using Jetbrains.Annotations;
 
 namespace TempDirectory.Test
 {
     public class TempDirectoryBuilderTest
     {
-        [Fact]
-        public void CreatesTempDirectory()
+        [Theory]
+        [MemberData(nameof(GetTempDirectoryBuilderConfigurations))]
+        public void CreatesTempDirectory(TempDirectoryBuilder tempDirectoryBuilder)
         {
-            var tempDirectoryBuilder = CreateTempDirectoryBuilder();
             using (var tempDirectory = tempDirectoryBuilder.Create())
             {
                 Assert.True(Directory.Exists(tempDirectory.FullName));
             }
         }
 
-        [Fact]
-        public void FullNameContainsName()
+        [Theory]
+        [MemberData(nameof(GetTempDirectoryBuilderConfigurations))]
+        public void DeletesTempDirectory(TempDirectoryBuilder tempDirectoryBuilder)
         {
-            var tempDirectoryBuilder = CreateTempDirectoryBuilder();
+            var tempDirectory = tempDirectoryBuilder.Create();
+            Assert.True(Directory.Exists(tempDirectory.FullName));
+            tempDirectory.Dispose();
+
+            Assert.False(Directory.Exists(tempDirectory.FullName));
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTempDirectoryBuilderConfigurations))]
+        public void FullNameContainsName(TempDirectoryBuilder tempDirectoryBuilder)
+        {
             using (var tempDirectory = tempDirectoryBuilder.Create())
             {
                 Assert.Contains(tempDirectory.FullName, tempDirectory.Name);
             }
         }
 
-        private static ITempDirectoryBuilder CreateTempDirectoryBuilder()
+        [Theory]
+        [MemberData(nameof(GetTempDirectoryBuilderConfigurations))]
+        public void FullNameIsNotName(TempDirectoryBuilder tempDirectoryBuilder)
         {
-            return new TempDirectoryBuilder();
+            using (var tempDirectory = tempDirectoryBuilder.Create())
+            {
+                Assert.NotEqual(tempDirectory.FullName, tempDirectory.Name);
+            }
         }
+
+        [UsedImplicitely]
+        public static IEnumerable<object[]> GetTempDirectoryBuilderConfigurations()
+        {
+            yield return new object[] { new TempDirectoryBuilder() };
+            yield return new object[] { new TempDirectoryBuilder().Prefix(Prefix) };
+            yield return new object[] { new TempDirectoryBuilder().Suffix(Suffix) };
+            yield return new object[] { new TempDirectoryBuilder().Prefix(Prefix).Suffix(Suffix) };
+            yield return new object[] { new TempDirectoryBuilder().Prefix(Prefix).PrefixSeparator(PrefixSeparator) };
+            yield return new object[] { new TempDirectoryBuilder().Suffix(Prefix).SuffixSeparator(SuffixSeparator) };
+            yield return new object[] { new TempDirectoryBuilder().Prefix(Prefix).PrefixSeparator(PrefixSeparator).Suffix(Prefix).SuffixSeparator(SuffixSeparator) };
+        }
+
+        private const string Prefix = "prefix";
+        private const string Suffix = "prefix";
+        private const string PrefixSeparator = "-";
+        private const string SuffixSeparator = "-";
     }
 }
